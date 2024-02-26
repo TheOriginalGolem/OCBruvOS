fa39c68709a1fe0f6118a0671400cd4f53b0b28e
Golem <63157010+TheOriginalGolem@users.noreply.github.com>
2024-02-26 02:13:24 +0200

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -3,12 +3,15 @@ local filesystem = require("filesystem")
 
 shell.execute("rm /home/Main.lua")
 shell.execute("rm /home/Password.lua")
+shell.execute("rm /home/Employee.lua")
 shell.execute("rm /home/DoorControl.lua")
 shell.execute("rm /home/Maintenance.lua")
 
 shell.execute("wget https://raw.githubusercontent.com/TheOriginalGolem/OCBruvOS/main/Main.lua /home/Main.lua")
 
 shell.execute("wget https://raw.githubusercontent.com/TheOriginalGolem/OCBruvOS/main/Password.lua /home/Password.lua")
+
+shell.execute("wget https://raw.githubusercontent.com/TheOriginalGolem/OCBruvOS/main/Employee.lua /home/Employee.lua")
 
 shell.execute("wget https://raw.githubusercontent.com/TheOriginalGolem/OCBruvOS/main/DoorControl.lua /home/DoorControl.lua")
 
