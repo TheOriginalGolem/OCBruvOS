afea64154220f13252a9b1ec15a1efb5768d3dec
Golem <63157010+TheOriginalGolem@users.noreply.github.com>
2025-02-20 10:26:49 +0200

    Update Updates.lua

--- Updates.lua
+++ Updates.lua
@@ -11,12 +11,22 @@ updateAvailable = false
 function updateChecker()
     local file = io.open("Changelog.txt", "r")
 
-    local currentVersion = file:read("4")
+    local currentVersion = file:read(4)
     file:close()
     print(currentVersion)
 
 end
 
+function viewChangelog()
+    local file = io.open("Changelog.txt", "r")
+
+    local changelogText = file:read("*a")
+    file:close()
+    clearScreen()
+    print(changelogText)
+    print("\n\nPress enter to close")
+    io.read()
+end
 
 function UpdateMenu()
     while true do
@@ -24,6 +34,7 @@ function UpdateMenu()
         print("Update Menu:")
         print("[1] Check for Updates")
         print("[2] Force System Update")
+        print("[3] View Changelog")
         print("[0] Exit")
 
         local selectedOption = read();
@@ -32,6 +43,8 @@ function UpdateMenu()
             os.sleep(1.5)
         elseif selectedOption == "2" then
             systemUpdate()
+        elseif selectedOption == "3" then
+            viewChangelog()
         elseif selectedOption == "0" then
             break
         else
